16c1f7bafc744edceb3c1cbf1608ccd0b716c178
Kənan İdayatov <idayatovkenan7@gmail.com>
2022-09-09 22:02:13 +0400

    Multiple Layout and route configure added

--- src/app/ui/components/home/home.module.ts
+++ src/app/ui/components/home/home.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
+import { BasketsComponent } from '../baskets/baskets.component';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -9,7 +11,10 @@ import { HomeComponent } from './home.component';
     HomeComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild([
+      { path: "", component: HomeComponent }
+    ])
   ]
 })
 export class HomeModule { }

8ff3c8e19211bd8c7da897b5a763e54f0025e18e
ASUS <idayatovkenan7@gmail.com>
2022-12-14 18:37:37 +0400

    spinner added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { CustomToastrService, ToastrMessageType, ToastrPosition } from './services/ui/custom-toastr.service';
 declare var $: any
 
 @Component({
@@ -10,19 +9,7 @@ declare var $: any
 export class AppComponent {
   title = 'ETicaretClient';
   
-  constructor(private toastrService: CustomToastrService){
-    toastrService.message("Merhaba", "Kenan", {
-      messageType: ToastrMessageType.Info, 
-      position: ToastrPosition.BottomRight
-    });
-    toastrService.message("Sagol", "Kenan", {
-      messageType: ToastrMessageType.Success, 
-      position: ToastrPosition.TopLeft
-    });
-    toastrService.message("Axsamin xeyir", "Kenan", {
-      messageType: ToastrMessageType.Error, 
-      position: ToastrPosition.BottomRight
-    });
+  constructor(){ 
   }
 }
 

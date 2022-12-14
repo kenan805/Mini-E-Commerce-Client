99858975bfcc076bf5357cfaba667efab64a997b
ASUS <idayatovkenan7@gmail.com>
2022-12-14 16:44:14 +0400

    Customized Notification added with Toastr for UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CustomToastrService, ToastrMessageType, ToastrPosition } from './services/ui/custom-toastr.service';
 declare var $: any
 
 @Component({
@@ -8,6 +9,21 @@ declare var $: any
 })
 export class AppComponent {
   title = 'ETicaretClient';
+  
+  constructor(private toastrService: CustomToastrService){
+    toastrService.message("Merhaba", "Kenan", {
+      messageType: ToastrMessageType.Info, 
+      position: ToastrPosition.BottomRight
+    });
+    toastrService.message("Sagol", "Kenan", {
+      messageType: ToastrMessageType.Success, 
+      position: ToastrPosition.TopLeft
+    });
+    toastrService.message("Axsamin xeyir", "Kenan", {
+      messageType: ToastrMessageType.Error, 
+      position: ToastrPosition.BottomRight
+    });
+  }
 }
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UiModule } from './ui/ui.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    UiModule, AdminModule, BrowserAnimationsModule
+    UiModule, AdminModule, BrowserAnimationsModule,
+    ToastrModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
